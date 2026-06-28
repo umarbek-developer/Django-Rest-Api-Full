c154517787b7b798364f960730a20d7b957be1aa
Umarsobirov <umarbeksobirov0402@mail.ru>
2026-06-28 10:18:01 +0500

    Implement API view for product retrieval and add ProductForm and sale_price property

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
+
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 
+
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
+
+    if request.method != "POST":
+        return Response({"detail": "GET not allowed!"}, status=405)
+
+
     model_data = Product.objects.all.order_by("?").first() 
     data = {}
 
     if model_data:
-        data['title'] = model_data.title  
-        data['content'] = model_data.content 
-        data['price'] - model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
